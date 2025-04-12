01ce0465a8f6ff327b15f16d47a67bdbb2c27275
Ilan Merkovich <merkovichilan@gmail.com>
2025-04-12 22:18:17 +0300

    finished constructor

--- src/Game/Engine/GameMap.java
+++ src/Game/Engine/GameMap.java
@@ -1,9 +1,6 @@
 package Game.Engine;
 
-import Game.Characters.Dragon;
-import Game.Characters.Goblin;
-import Game.Characters.Orc;
-import Game.Characters.PlayerCharacter;
+import Game.Characters.*;
 import Game.Core.GameEntity;
 import Game.Items.Potion;
 import Game.Items.PowerPotion;
@@ -14,32 +11,47 @@ import java.util.*;
 
 public class GameMap{
     private Map<Position,List<GameEntity>>grid;
-    public GameMap(int rows,int cols,String name){
-        this.grid=new HashMap<>();
-        Random random=new Random();
-        for (int i = 0; i < rows; i++){
-            for (int j = 0; j < cols; j++){
-                Position newPos=new Position(i,j);
-                GameEntity entity=generateEntity(i,j,random);
-                List<GameEntity>entityCell=new ArrayList<>();
-                if (entity!=null){
+    public GameMap(int rows, int cols, String name, int type) {
+        this.grid = new HashMap<>();
+        Random random = new Random();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                Position newPos = new Position(i, j);
+                GameEntity entity = generateEntity(i, j, random);
+                List<GameEntity> entityCell = new ArrayList<>();
+                if (entity != null) {
                     entity.setPosition(newPos);
                     entityCell.add(entity);
                 }
-                grid.put(newPos,entityCell);
+                grid.put(newPos, entityCell);
             }
-            while (true){
-                int r=random.nextInt(rows);
-                int c=random.nextInt(cols);
-                Position playerPosition=new Position(r,c);
-                List<GameEntity>Cell=grid.get(playerPosition);
-                if (Cell.isEmpty()){
-
+        }
+        while (true) {
+            int r = random.nextInt(rows);
+            int c = random.nextInt(cols);
+            Position playerPos=new Position(r,c);
+            List<GameEntity>cell=grid.get(playerPos);
+            if (cell.isEmpty()) {
+                PlayerCharacter player = null;
+                if (type == 1) {
+                    player=new Warrior(r,c,name);
                 }
+                else if (type == 2) {
+                    player=new Mage(r,c,name);
+                }
+                else if (type == 3) {
+                    player=new Archer(r,c,name);
+                }
+                else {
+                    System.out.println("ERROR: Invalid player type.");
+                    break;
+                }
+                player.setPosition(playerPos);
+                cell.add(player);
+                break;
             }
         }
     }
-    private PlayerCharacter create
     private GameEntity generateEntity(int row,int col,Random random){
         int probability=random.nextInt(100);
         if (probability<40){
--- src/Game/Engine/GameWorld.java
+++ src/Game/Engine/GameWorld.java
@@ -1,6 +1,7 @@
 package Game.Engine;
 import Game.Characters.Enemy;
 import Game.Characters.PlayerCharacter;
+import Game.Core.GameEntity;
 import Game.Items.GameItem;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,10 +11,10 @@ public class GameWorld{
     private List<Enemy>enemies;
     private List<GameItem>items;
     private GameMap map;
-    public GameWorld(int rows,int cols,String name){
+    public GameWorld(int rows,int cols,String name,int type){
         this.players=new ArrayList<PlayerCharacter>();
         this.enemies=new ArrayList<Enemy>();
         this.items=new ArrayList<GameItem>();
-        this.map=new GameMap(rows,cols,name);
+        this.map=new GameMap(rows,cols,name,type);
     }
 }
